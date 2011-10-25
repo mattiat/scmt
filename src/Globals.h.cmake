58409c86a2e8bf37e043e6642de0b8ce6797a795
Mattia Tomasoni <tomasonimattia@gmail.com>
2011-10-25 18:49:48 +0100

    adding config var in CMakeLists

---
+++ src/Globals.h.cmake
@@ -0,0 +1,30 @@
+//============================================================================
+// Name        : TranscriptionNetwork
+// Author      : Mattia Tomasoni
+// Institution : University of Leeds
+//============================================================================
+
+#ifndef GLOBALS_H_
+#define GLOBALS_H_
+
+#ifndef MAX_TIME
+#define MAX_TIME @MAX_TIME@
+#endif
+#ifndef EULER_STEP
+#define EULER_STEP @EULER_STEP@
+#endif
+#ifndef VERION_MINOR
+#define VERION_MINOR @VERSION_MINOR@
+#endif
+#ifndef VERION_MAJOR
+#define VERION_MAJOR @VERSION_MAJOR@
+#endif
+#ifndef WRITABLE_DIR
+#define WRITABLE_DIR "@WRITABLE_DIR@"
+#endif
+#ifndef TEMP_DIR
+#define TEMP_DIR "@TEMP_DIR@"
+#endif
+
+
+#endif /* GLOBALS_H_ */
